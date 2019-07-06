241f4ea82faceed88767bddec20d11f36115bf92
Candace Joy Metsker <candacejmetsker@gmail.com>
2019-07-06 17:36:57 -0400

    turning point for AddItems

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
+
+
 function addItem()  {
     //when AddItem is pressed
     $("#js-shopping-list-form").submit(function (event) {
        //prevent default
         event.preventDefault();
-        //clear text from input variable used previously - MAY NOT NEED THIS??
-        //$("#shopping-list-entry").emtpy();
-        //create variable - take the text from the input and store it in the variable
-        const newItem = $(this).find(input[name="shopping-list-entry"]).val();
-        //create a new child object - add it to class Shopping List
-        $('.shopping-list').append(newItem);
+
+        //create a new li element
+
+        let newItemName = $(this).find(input[name="shopping-list-entry"]).val();
+        
+        //create a new child by cloning?
+        let newItem = document.createElement(`li`)
+
+        let newItemSpan = document.createElement('span');
+        newItemSpan.textContent = newItemName;
+        newItemSpan.className = "shopping-item";
+
+        //create div for buttons
+        let newItemDiv = document.createElement('div');
+        newItemsDiv.classNames = "shopping-item-controls";
+
+        //create checkbutton
+        let checkButton = document.createElement("button");
+        checkButton.className = "shopping-item-toggle";
+
+        let checkButtSpan = document.createElement("span")
+        checkButton.textContent = "check";
+        checkButton.appendChild(checkButtSpan);
+
+        //create delete button
+        let deleteButton = document.createElement("button");
+        deleteButton.className = "shopping-item-delete";
+        let deleteButtSpan = document.createElement("span");
+        deleteButtSpan.textContent = "delete";
+
+        //add buttons to div
+        newItemsDiv.appendChild(checkButton, deleteButton);
+
+        newItem.appendChild(newItemSpan, newItemDiv);
+
+        //add new list item to list
+        $('ul .shopping-list').prepend(newItem);
 
     });
 }
@@ -20,7 +53,6 @@ function checkItem()  {
        event.preventDefault();
 
         //needs to add "_checked" to the class name of list item
-
         $(this).parent().siblings(`.shopping-item`).toggleClass(" shopping-item__checked");
     });
 }
