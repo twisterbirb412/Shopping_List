c8b2b0987a81d558c1b84d42ed83ead695c437de
Candace Joy Metsker <candacejmetsker@gmail.com>
2019-07-07 08:11:14 -0400

    final product

--- index.js
+++ index.js
@@ -2,63 +2,45 @@
 
 function addItem()  {
     //when AddItem is pressed
-    $("#js-shopping-list-form").submit(function (event) {
+    $('#js-shopping-list-form').submit(event => {
        //prevent default
         event.preventDefault();
 
-        //create a new li element
+        //retrieves list item name from input
+        let newItemName = $('#js-shopping-list-form').find(`input[name="shopping-list-entry"]`).val();
 
-        let newItemName = $(this).find(input[name="shopping-list-entry"]).val();
-        
-        //create a new child by cloning?
-        let newItem = document.createElement(`li`)
+        let htmlText = 
+        `<span class="shopping-item">`+ newItemName+ `</span>` +
+        `<div class="shopping-item-controls">` + 
+          `<button class="shopping-item-toggle">` + 
+            `<span class="button-label">check</span>` + 
+          `</button>` + 
+         ` <button class="shopping-item-delete">` +
+            `<span class="button-label">delete</span>` + 
+          `</button>` + 
+        `</div>`;
 
-        let newItemSpan = document.createElement('span');
-        newItemSpan.textContent = newItemName;
-        newItemSpan.className = "shopping-item";
+        $(".shopping-list").append("<li>" + htmlText + "</li>");
 
-        //create div for buttons
-        let newItemDiv = document.createElement('div');
-        newItemsDiv.classNames = "shopping-item-controls";
-
-        //create checkbutton
-        let checkButton = document.createElement("button");
-        checkButton.className = "shopping-item-toggle";
-
-        let checkButtSpan = document.createElement("span")
-        checkButton.textContent = "check";
-        checkButton.appendChild(checkButtSpan);
-
-        //create delete button
-        let deleteButton = document.createElement("button");
-        deleteButton.className = "shopping-item-delete";
-        let deleteButtSpan = document.createElement("span");
-        deleteButtSpan.textContent = "delete";
-
-        //add buttons to div
-        newItemsDiv.appendChild(checkButton, deleteButton);
-
-        newItem.appendChild(newItemSpan, newItemDiv);
-
-        //add new list item to list
-        $('ul .shopping-list').prepend(newItem);
+        $('#js-shopping-list-form').find(`input[name="shopping-list-entry"]`).val("");
 
     });
 }
 
 
 function checkItem()  {
-    $(".shopping-item-toggle").click(function(event) {
+    $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
        //prevent default
        event.preventDefault();
 
-        //needs to add "_checked" to the class name of list item
+        //needs to add "_checked" to the class name of list items
         $(this).parent().siblings(`.shopping-item`).toggleClass(" shopping-item__checked");
     });
+
 }
 
 function deleteItem()  {
-    $(".shopping-item-delete").click(function(event) {
+    $(".shopping-list").on("click", ".shopping-item-delete", function(event) {
         //prevents default
         event.preventDefault();
 
